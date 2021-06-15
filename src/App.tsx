ae8f7292b7881549094a8678235bf10513707b69
luca <lucaperullo@outlook.it>
2021-06-15 14:44:55 +0200

    ionic temp

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,43 @@
-import React from 'react'
-import { Route, Redirect, RouteComponentProps } from 'react-router-dom'
-import { IonRouterOutlet, IonPage, IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/react'
-import { home, apps } from 'ionicons/icons'
-import { Helmet } from 'react-helmet'
-import Home from './pages/Home'
-import User from './pages/User'
-import About from './pages/About'
+import { Redirect, Route } from "react-router-dom";
+import { IonApp, IonItem, IonIcon, IonLabel, IonToggle } from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router";
+import { moon } from "ionicons/icons";
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css'
+import "@ionic/react/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css'
-import '@ionic/react/css/structure.css'
-import '@ionic/react/css/typography.css'
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css'
-import '@ionic/react/css/float-elements.css'
-import '@ionic/react/css/text-alignment.css'
-import '@ionic/react/css/text-transformation.css'
-import '@ionic/react/css/flex-utils.css'
-import '@ionic/react/css/display.css'
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
 
 /* Theme variables */
-import './theme/variables.css'
+import "./theme/variables.css";
+const toggleDarkModeHandler = () => {
+  document.body.classList.toggle("dark");
+};
+const App: React.FC = () => (
+  <IonApp>
+    <IonReactRouter>
+      <IonItem>
+        <IonIcon slot="end" icon={moon} />
+        <IonLabel>Dark Mode</IonLabel>
+        <IonToggle
+          slot="end"
+          name="darkMode"
+          onIonChange={toggleDarkModeHandler}
+        />
+      </IonItem>
+    </IonReactRouter>
+  </IonApp>
+);
 
-const App: React.FunctionComponent<RouteComponentProps> = props => {
-  // hide the tab bar when not on home or about page
-  const display = props.location.pathname.match(/home$|about$/g) ? 'flex' : 'none'
-
-  return (
-    <IonPage id="main">
-      {/*using Helmet to add the css to the head*/}
-      <Helmet>
-        <style type="text/css">{`
-        ion-tab-bar {
-            display: ${display};
-        }
-    `}</style>
-      </Helmet>
-      <IonTabs>
-        <IonRouterOutlet>
-          <Route path="/home" component={Home} exact />
-          <Route path="/home/:user" component={User} exact />
-          <Route path="/about" component={About} exact />
-          <Redirect exact from="/" to="/home" />
-        </IonRouterOutlet>
-
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="home" href="/home">
-            <IonIcon icon={home} />
-            <IonLabel>Home</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="about" href="/about">
-            <IonIcon icon={apps} />
-            <IonLabel>About</IonLabel>
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
-    </IonPage>
-  )
-}
-
-export default App
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import { IonApp } from '@ionic/react'
-import { IonReactRouter } from '@ionic/react-router'
-import { Route } from 'react-router'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <IonReactRouter>
-    <IonApp>
-      <Route path="/" component={App} />
-    </IonApp>
-  </IonReactRouter>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
-)
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
