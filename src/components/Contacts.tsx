6bf7d3fae1a0a3b2dfae9859836db2b69f5b6073
ABskr <bskr26@gmx.de>
2021-06-16 16:02:49 +0200

    fix scrollbar

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import { IonContent, IonItem, IonAvatar, IonLabel } from '@ionic/react';
-import styled from 'styled-components';
+
 import '../theme/style.css'
 
 const Contacts: React.FC = () => (
   <IonContent
-  id="#style-3"
-  className="scrollbar"
   fullscreen
     scrollEvents={true}
     onIonScrollStart={() => {}}
     onIonScroll={() => {}}
     onIonScrollEnd={() => {}}>
-      <div className="force-overflow"></div>
       <IonItem>
       <IonAvatar slot="start">
         <img src="https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y" alt="profileImg" />
