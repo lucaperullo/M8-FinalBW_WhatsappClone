6408bbc30c59115ed5df243403e95052c9e8636d
luca <lucaperullo@outlook.it>
2021-06-20 00:21:10 +0200

    up

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,15 +1,15 @@
-import axios from "axios"
+import axios from "axios";
 
-let baseURL = ""
+let baseURL = "";
 
 if (process.env.NODE_ENV !== "production") {
-  baseURL = "http://localhost:5000"
+  baseURL = "https://whatzappy.herokuapp.com/";
 } else {
-  baseURL = process.env.REACT_APP_PROD || ""
+  baseURL = process.env.REACT_APP_PROD || "";
 }
 
 const backend = axios.create({
   baseURL,
-})
+});
 
-export { backend }
+export { backend };
